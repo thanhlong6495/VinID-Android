01e7c126d436535e364eecdedbcb724e6297d45e
Long White <long.instulink@gmail.com>
2021-04-01 10:16:32 +0900

    fix branch

---
+++ app/src/main/java/com/longtrang/vinid/PhoneNumberRegister.java
@@ -0,0 +1,62 @@
+package com.longtrang.vinid;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterForm extends AppCompatActivity {
+    TextView textViewPhoneNumber, textViewInvitationCode,textViewPolicy;
+    EditText editTextPhoneNumber;
+    Button buttonNext;
+    String phoneNumber;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_form);
+        mappingView();
+        editTextPhoneNumber.requestFocus();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE|WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        Intent intent = getIntent();
+
+    }
+
+    public void mappingView(){
+        textViewPhoneNumber     = findViewById(R.id.tv_enter_phonenumber_register);
+        textViewInvitationCode  = findViewById(R.id.tv_invitation_code);
+        textViewPolicy          = findViewById(R.id.tv_policy);
+        editTextPhoneNumber     = findViewById(R.id.edt_phonenumber_register);
+        buttonNext              = findViewById(R.id.btn_next);
+    }
+
+    public void clickOnInvitationCode(View view) {
+        Dialog dialog = new Dialog(RegisterForm.this);
+        dialog.setContentView(R.layout.dialog_invitation_code);
+        dialog.show();
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+    }
+
+    public void goToConfirmOTP(View view) {
+        phoneNumber = editTextPhoneNumber.getText().toString().trim();
+        if(phoneNumber.isEmpty()){
+            Toast.makeText(this, getResources().getString(R.string.phone_number_warning), Toast.LENGTH_LONG).show();
+        }
+        else{
+            Intent intent = new Intent(RegisterForm.this, ConfirmOTP.class);
+            intent.putExtra("phone number",phoneNumber);
+            startActivity(intent);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/longtrang/vinid/PinCode.java
@@ -0,0 +1,14 @@
+package com.longtrang.vinid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PinCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_code);
+    }
+}
