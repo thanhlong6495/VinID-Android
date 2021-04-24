35366c6ae9990434afed138bf98b42a1e159d419
Long White <long.instulink@gmail.com>
2021-04-24 12:21:52 +0900

    Add CountDownTimer for OTP Activity

--- app/src/main/java/Activities/ConfirmOTP.java
+++ app/src/main/java/Activities/ConfirmOTP.java
@@ -4,52 +4,86 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.longtrang.vinid.R;
+import static com.longtrang.vinid.R.*;
 
 public class ConfirmOTP extends AppCompatActivity {
-    TextView textViewPhonenumberChange;
-    TextView textViewOTPSendAgain;
-    TextView textViewOTPGuide;
-    EditText editTextOTP;
-    Button buttonNext;
+    private TextView textViewPhonenumberChange;
+    private TextView textViewOTPSendAgain;
+    private TextView textViewOTPGuide;
+    private TextView textViewCountDownTimer;
+    private EditText editTextOTP;
+    private Button buttonNext;
     String phoneNumber;
+    private CountDownTimer countDownTimer;
+    private int remainingTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.confirm_otp);
+        setContentView(layout.confirm_otp);
         assigningView();
         editTextOTP.requestFocus();
         getData();
+        setCountDownTimer();
+        textViewOTPSendAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.cancel();
+                setCountDownTimer();
+            }
+        });
     }
-
     public void assigningView() {
-        textViewOTPGuide            = findViewById(R.id.tv_enter_otp_guide);
-        textViewPhonenumberChange   = findViewById(R.id.tv_phonenumber_change);
-        textViewOTPSendAgain        = findViewById(R.id.tv_OTP_send_again);
-        editTextOTP                 = findViewById(R.id.edt_otp_code);
-        buttonNext                  = findViewById(R.id.btn_otp_confirm);
+        textViewCountDownTimer      = findViewById(id.tv_countdown_timer);
+        textViewOTPGuide            = findViewById(id.tv_enter_otp_guide);
+        textViewPhonenumberChange   = findViewById(id.tv_phonenumber_change);
+        textViewOTPSendAgain        = findViewById(id.tv_OTP_send_again);
+        editTextOTP                 = findViewById(id.edt_otp_code);
+        buttonNext                  = findViewById(id.btn_otp_confirm);
     }
     public void getData() {
         Intent intent = getIntent();
         phoneNumber = intent.getStringExtra("phonenumber");
-        textViewOTPGuide.setText(getResources().getString(R.string.enter_otp_code) + " " + phoneNumber);
+        textViewOTPGuide.setText(getResources().getString(string.enter_otp_code) + " " + phoneNumber);
     }
     public void goToPinCode(View view) {
-        if (editTextOTP.length() == getResources().getInteger(R.integer.otp_code)) {
+        if (editTextOTP.length() == getResources().getInteger(integer.otp_code)) {
             startActivity(new Intent(ConfirmOTP.this, PinCode.class));
         }
         else {
-            Toast.makeText(this, getResources().getString(R.string.otp_code_warning), Toast.LENGTH_LONG).show();
+            Toast.makeText(this, getResources().getString(string.otp_code_warning), Toast.LENGTH_LONG).show();
         }
     }
     public void backToPhoneNumberRegister(View view) {
         startActivity(new Intent(ConfirmOTP.this, PhoneNumberRegister.class));
+    }
+    private void setCountDownTimer() {
+        countDownTimer = new CountDownTimer(getResources().getInteger(integer.otp_countdowntimer), getResources().getInteger(integer.convert_time)) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                editTextOTP.setFocusableInTouchMode(true);
+                remainingTime = (int) millisUntilFinished;
+                updateTime();
+            }
+            @Override
+            public void onFinish() {
+                editTextOTP.setFocusable(false);
+                countDownTimer.cancel();
+                Toast.makeText(ConfirmOTP.this, getResources().getString(string.otp_time_warning), Toast.LENGTH_LONG).show();
+            }
+        };
+        countDownTimer.start();
+    }
+    private void updateTime() {
+        int minutes = (remainingTime / 1000) / 60;
+        int seconds = (remainingTime /1000) % 60;
+        String timeFormatted = String.format("%02d:%02d", minutes, seconds);
+        textViewCountDownTimer.setText(timeFormatted);
     }
 }
