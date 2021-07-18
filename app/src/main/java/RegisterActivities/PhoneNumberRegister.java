035bfb77f9c1cd3064cfae2583b2e547ae6aea88
Long White <long.instulink@gmail.com>
2021-07-18 14:35:36 +0900

    Fix PhoneNumber Activity

--- app/src/main/java/RegisterActivities/PhoneNumberRegister.java
+++ app/src/main/java/RegisterActivities/PhoneNumberRegister.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
@@ -46,20 +45,18 @@ public class PhoneNumberRegister extends AppCompatActivity {
     }
     public void goToConfirmOTP(View view) {
         if (editTextPhoneNumber.length() < getResources().getInteger(R.integer.phone_number)) {
-            phoneNumber = editTextPhoneNumber.getText().toString().trim();
-            if (phoneNumber.length() < getResources().getInteger(R.integer.phone_number)) {
-                Toast.makeText(this, getResources().getString(R.string.phone_number_warning), Toast.LENGTH_LONG).show();
+            Toast.makeText(this, getResources().getString(R.string.phone_number_warning), Toast.LENGTH_LONG).show();
             }
-            else {
-                intent = new Intent(PhoneNumberRegister.this, ConfirmOTP.class);
-                intent.putExtra("phonenumber", phoneNumber);
-                startActivity(intent);
-                SharedPreferences sharedPreferences = getSharedPreferences("userInformation",
+        else {
+            phoneNumber = editTextPhoneNumber.getText().toString().trim();
+            intent = new Intent(PhoneNumberRegister.this, ConfirmOTP.class);
+            intent.putExtra("phonenumber", phoneNumber);
+            startActivity(intent);
+            SharedPreferences sharedPreferences = getSharedPreferences("userInformation",
                         MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("phonenumber", phoneNumber);
-                editor.apply();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("phonenumber", phoneNumber);
+            editor.apply();
             }
         }
     }
-}
